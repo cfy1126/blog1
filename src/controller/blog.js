1dd5fe8fdf00d58ea9f9494c0031f50ee78aa74a
linkincao <linkincao1988@gmail.com>
2023-10-26 21:58:28 +0800

    feat: api对接mysql

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,28 +1,17 @@
+const { exec } = require('../db/mysql');
+
 const getList = (author, keyword) => {
-    // 先返回假数据 （格式是正确）
-    return [
-        {
-            id: 1,
-            title: "标题A",
-            content: "内容A",
-            createTime: 1546610491112,
-            author: "zhangsan",
-        },
-        {
-            id: 2,
-            title: "标题B",
-            content: "内容B",
-            createTime: 1546610491112,
-            author: "lisi",
-        },
-        {
-            id: 3,
-            title: "标题C",
-            content: "内容C",
-            createTime: 1546610491112,
-            author: "wangwu",
-        },
-    ];
+    let sql = `select * from blogs where 1=1 `;
+    if (author) {
+        sql += `and author=${author}`;
+    }
+    if (keyword) {
+        sql += `and title like '%${keyword}%`;
+    }
+    sql += `order by createtime desc;`;
+
+    // 返回promise
+    return exec(sql);
 };
 
 const getDetail = (id) => {
