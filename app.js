95c376c935d12320255a545ff321a7e515f19a1a
linkincao <linkincao1988@gmail.com>
2023-10-29 12:58:06 +0800

    feat: session

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const querystring = require("querystring");
 const handleBlogRouter = require("./src/router/blog");
 const handleUserRouter = require("./src/router/user");
 
+// 获取cookie的过期时间
+const getCookieExpires = ()=>{
+    const d = new Date();
+    d.setTime(d.getTime() + (24*60*60*1000));
+    console.log('d.toGMTString() is ', d.toGMTString());
+    // GMT格式
+    return d.toGMTString();
+}
+
+// session 数据
+const SESSION_DATA = {};
+
 // 用于处理 post data
 const getPostData = (req) => {
     const promise = new Promise((resolve, reject) => {
@@ -42,15 +54,29 @@ const serverHandle = (req, res) => {
     req.cookie = {};
     const cookieStr = req.headers.cookie || ''; // k1=v1;k2=v2;k3=v3
     cookieStr.split(';').forEach(item => {
-        if(!item){
+        if (!item) {
             return;
         }
         const arr = item.split('=');
         const key = arr[0].trim();
         const val = arr[1].trim();
         req.cookie[key] = val;
-        console.log(arr);
     })
+
+    // 解析session
+    let needSetCookie = false;
+    let userId = req.cookie.userId;
+    if (userId) {
+        if (!SESSION_DATA[userId]) {
+            SESSION_DATA[userId] = {};
+        } 
+    }else{
+        needSetCookie = true;
+        userId = `${Date.now()}_${Math.random()}`;
+        SESSION_DATA[userId] = {};
+    }
+    req.session = SESSION_DATA[userId] || {};
+    
 
     // 处理 post data
     getPostData(req).then((postData) => {
@@ -59,6 +85,9 @@ const serverHandle = (req, res) => {
         const blogResult = handleBlogRouter(req, res);
         if (blogResult) {
             blogResult.then(blogData => {
+                if(needSetCookie){
+                    res.setHeader('Set-Cookie',`userId=${userId};path=/;httpOnly;expires=${getCookieExpires()}`)
+                }
                 res.end(JSON.stringify(blogData));
             })
             return;
@@ -68,6 +97,9 @@ const serverHandle = (req, res) => {
         const userData = handleUserRouter(req, res);
         if (userData) {
             userData.then(userData => {
+                if(needSetCookie){
+                    res.setHeader('Set-Cookie',`userId=${userId};path=/;httpOnly;expires=${getCookieExpires()}`)
+                }
                 res.end(JSON.stringify(userData));
             })
             return;
--- src/router/user.js
+++ src/router/user.js
@@ -1,14 +1,6 @@
 const { login } = require('../controller/user.js')
 const { SuccessModel, ErrorModel } = require('../model/resModel.js')
 
-// 获取cookie的过期时间
-const getCookieExpires = () => {
-    const d = new Date();
-    d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
-    console.log('d.toGMTString() is ', d.toGMTString());
-    // GMT格式
-    return d.toGMTString();
-}
 const handleUserRouter = (req, res) => {
     const method = req.method;
 
@@ -19,8 +11,9 @@ const handleUserRouter = (req, res) => {
         const result = login(username, password);
         return result.then(data => {
             if (data.username) {
-                // 操作cookie
-                res.setHeader('Set-Cookie', `username=${data.username};path=/;httpOnly;expires=${getCookieExpires()}`)
+                // 设置session
+                req.session.username = data.username;
+                req.session.realname = data.realname;
                 return new SuccessModel();
             } else {
                 return new ErrorModel('登录失败');
@@ -29,12 +22,12 @@ const handleUserRouter = (req, res) => {
     }
 
     // 登录验证测试
-    if (method === 'GET' && req.path === '/api/user/login-test') {
-        if (req.cookie.username) {
+    if(method === 'GET' && req.path === '/api/user/login-test'){
+        if(req.session.username){
             return Promise.resolve(new SuccessModel({
-                username: req.cookie.username
+                session: req.session
             }));
-        } else {
+        }else{
             return Promise.resolve(new ErrorModel('尚未登录'));
         }
     }
