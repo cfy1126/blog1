3fda4f7f308df5e3c47fe3d51886a875173f9071
linkincao <linkincao1988@gmail.com>
2023-11-05 11:26:09 +0800

    nginx反向代理与连调

--- src/router/blog.js
+++ src/router/blog.js
@@ -3,7 +3,7 @@ const { SuccessModel, ErrorModel } = require('../model/resModel');
 
 // 统一的登录验证函数
 const loginCheck = (req) => {
-    if (req.session.username) {
+    if (!req.session.username) {
         return Promise.resolve(new ErrorModel('尚未登录'));
     }
 }
@@ -14,8 +14,18 @@ const handleBlogRouter = (req, res) => {
 
     // 获取博客列表
     if (method === 'GET' && req.path === '/api/blog/list') {
-        const author = req.query.author || '';
+        let author = req.query.author || '';
         const keyword = req.query.keyword || '';
+        if(req.query.isadmin){
+            // 管理员界面
+            const loginCheckResult = loginCheck(req);
+            if(loginCheckResult){
+                // 未登录
+                return loginCheckResult;
+            }
+            // 强制查询自己的博客
+            author = req.session.username;
+        }
         const result = getList(author, keyword);
         return result.then(listData => {
             return new SuccessModel(listData);
@@ -30,11 +40,10 @@ const handleBlogRouter = (req, res) => {
     }
     // 新增博客
     if (method === 'POST' && req.path === '/api/blog/new') {
-
         const loginCheckResult = loginCheck(req);
         if(loginCheckResult) {
             // 未登录
-            return loginCheck;
+            return loginCheckResult;
         }
         req.body.author = req.session.username;
         const result = newBlog(req.body);
@@ -47,7 +56,7 @@ const handleBlogRouter = (req, res) => {
         const loginCheckResult = loginCheck(req);
         if(loginCheckResult) {
             // 未登录
-            return loginCheck;
+            return loginCheckResult;
         }
         const result = updateBlog(id, req.body);
         return result.then(val => {
@@ -63,7 +72,7 @@ const handleBlogRouter = (req, res) => {
         const loginCheckResult = loginCheck(req);
         if(loginCheckResult) {
             // 未登录
-            return loginCheck;
+            return loginCheckResult;
         }
         const author = req.session.username;
         const result = delBlog(id, author);
